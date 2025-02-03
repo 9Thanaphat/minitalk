0d2766a08be7b592cbacdfd37e88e18687723dce
Thanaphat Tangcham <ttangcha@z2t5c1.42bangkok.com>
2025-02-03 14:20:29 +0700

    fix norm

--- client.c
+++ client.c
@@ -1,48 +1,58 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   client.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ttangcha <ttangcha@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/03 14:13:32 by ttangcha          #+#    #+#             */
+/*   Updated: 2025/02/03 14:16:20 by ttangcha         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <signal.h>
 #include <unistd.h>
 #include "libft.h"
 
-void send_bit(int pid, unsigned char ch)
+void	send_bit(int pid, unsigned char ch)
 {
-    int i;
+	int	i;
 
-    i = 7;
-    while (i >= 0)
-    {
-        if ((ch >> i) & 1)
-            kill(pid, SIGUSR2);
-        else
-            kill(pid, SIGUSR1);
-        usleep(100);
-        i--;
-    }
+	i = 7;
+	while (i >= 0)
+	{
+		if ((ch >> i) & 1)
+			kill(pid, SIGUSR2);
+		else
+			kill(pid, SIGUSR1);
+		usleep(100);
+		i--;
+	}
 }
 
-void str_to_bin(int pid, char *str)
+void	str_to_bin(int pid, char *str)
 {
-    int i;
+	int	i;
 
-    i = 0;
-    while (str[i])
-    {
-        send_bit(pid, str[i]);
-        i++;
-    }
-    send_bit(pid, '\0');
+	i = 0;
+	while (str[i])
+	{
+		send_bit(pid, str[i]);
+		i++;
+	}
+	send_bit(pid, '\0');
 }
 
-int main(int ac, char **av)
+int	main(int ac, char **av)
 {
-    if (ac == 3)
-    {
-        str_to_bin(ft_atoi(av[1]), av[2]);
-        ft_putstr_fd("Message sent to PID: ", 1);
-        ft_putnbr_fd(ft_atoi(av[1]), 1);
-        ft_putstr_fd("\n", 1);
-    }
-    else
-    {
-        ft_putstr_fd("Usage: ./server <PID> <message>\n", 1);
-    }
-    return 0;
+	if (ac == 3)
+	{
+		str_to_bin(ft_atoi(av[1]), av[2]);
+		ft_putstr_fd("Message sent to PID: ", 1);
+		ft_putnbr_fd(ft_atoi(av[1]), 1);
+		ft_putstr_fd("\n", 1);
+	}
+	else
+		ft_putstr_fd("Usage: ./client <PID> <message>\n", 1);
+	return (0);
 }
--- server.c
+++ server.c
@@ -1,41 +1,50 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   server.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ttangcha <ttangcha@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/03 14:13:37 by ttangcha          #+#    #+#             */
+/*   Updated: 2025/02/03 14:15:37 by ttangcha         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <signal.h>
 #include <unistd.h>
 #include "libft.h"
 
-static  void get_bit(int sig)
+static void	get_bit(int sig)
 {
-    static int i = 0;
-    static unsigned char ch = 0;
-    int bit;
+	static int				i = 0;
+	static unsigned char	ch = 0;
+	int						bit;
 
-    if (sig == SIGUSR1)
-        bit = 0;
-    else if (sig == SIGUSR2)
-        bit = 1;
-
-    ch = (ch << 1) | (bit & 1);
-    if (++i == 8)
-    {
-        if (ch == 0)
-        {
-            i = 0;
-            return;
-        }
-
-        write(1, &ch, 1);
-        i = 0;
-    }
+	if (sig == SIGUSR1)
+		bit = 0;
+	else if (sig == SIGUSR2)
+		bit = 1;
+	ch = (ch << 1) | (bit & 1);
+	if (++i == 8)
+	{
+		if (ch == 0)
+		{
+			i = 0;
+			return ;
+		}
+		write(1, &ch, 1);
+		i = 0;
+	}
 }
 
-int main()
+int	main(void)
 {
-    signal(SIGUSR1, get_bit);
-    signal(SIGUSR2, get_bit);
-    ft_putstr_fd("Server PID : ", 1);
-    ft_putnbr_fd(getpid(),1);
-    ft_putstr_fd("\n", 1);
-    while (1) {
-        pause();
-    }
-    return 0;
+	signal(SIGUSR1, get_bit);
+	signal(SIGUSR2, get_bit);
+	ft_putstr_fd("Server PID : ", 1);
+	ft_putnbr_fd(getpid(), 1);
+	ft_putstr_fd("\n", 1);
+	while (1)
+		pause();
+	return (0);
 }
